ba6bc938961a689a35df47dc1ed9b4a4c8930a58
Giacomo Rapposelli <rapposelli.giacomo@gmail.com>
2020-10-28 18:06:05 +0100

    fixed redirect bug

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/contact", (req, res) => {
         req.body.subject,
         `From: ${req.body.name}     ${req.body.email} Text: ${req.body.text}`
     )
-        .then(() => res.json(req.body))
+        .then(() => res.status(204).send())
         .catch((err) => console.log(err));
 });
 
